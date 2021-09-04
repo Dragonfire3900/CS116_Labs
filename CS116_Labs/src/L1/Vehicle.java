a601ce5bfe49a98792acc44690586e130389ce00
tangentialcubic <tangentialcubic@gmail.com>
2021-09-04 08:16:05 -0500

    Added our vehicle progress

--- CS116_Labs/src/L1/Vehicle.java
+++ CS116_Labs/src/L1/Vehicle.java
@@ -1,5 +1,35 @@
 package L1;
 
 public class Vehicle {
+    private int uniqueID;
+    private int arrivalTime;
+    private int tollBoothTime;
+    private int leavingTime;
+
+    static private int numVehicles = 0;
+
+    public Vehicle() {
+        numVehicles++;
+        uniqueID = numVehicles;
+        arrivalTime = 0;
+        tollBoothTime = 0;
+        leavingTime = 0;
+    }
+
+    public Vehicle(int aT) {
+        numVehicles++;
+        uniqueID = numVehicles;
+        setArrivalTime(aT);
+        tollBoothTime = 0;
+        leavingTime = 0;
+    }
+
+    public boolean setArrivalTime(int aT) {
+        if(aT <= leavingTime) {
+            arrivalTime = aT;
+            return true;
+        }
+        else return false;
+    }
 
 }
