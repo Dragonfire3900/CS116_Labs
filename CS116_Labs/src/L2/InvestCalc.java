f76fc80659c37444da87f2236972d7a63d98c3c1
tangentialcubic <tangentialcubic@gmail.com>
2021-09-08 14:52:58 -0500

    Set up skeleton for Invest Calc

--- CS116_Labs/src/L2/InvestCalc.java
+++ CS116_Labs/src/L2/InvestCalc.java
@@ -2,4 +2,43 @@ package L2;
 
 public class InvestCalc {
 
+	static final public int shortTerm = 5;
+	static final public int midTerm = 10;
+	static final public int longTerm = 20;
+	
+	static final private String cashFormatString = "";
+	static final private String percentFormatString = "";
+	
+	private double principal;
+	private double interestRate;
+
+	public InvestCalc() {
+		
+	}
+	
+	public InvestCalc(double interestRate, double principal) {
+		
+	}
+	
+	/*  --------------------------------
+	 *  |      Accessor Methods        |
+	 *  --------------------------------
+	 */
+	
+	/*  --------------------------------
+	 *  |       Mutator Methods        |
+	 *  --------------------------------
+	 */
+	
+	public String toString() {
+		
+	}
+	
+	public double futureValue(int year) {
+		
+	}
+	
+	public void displayTable() {
+		
+	}
 }
