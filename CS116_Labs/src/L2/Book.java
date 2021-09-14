6b683682ee1b39389b41960adb975d647adc1d64
August Gross <agross4@hawk.iit.edu>
2021-09-14 18:45:54 -0500

    update

--- CS116_Labs/src/L2/Book.java
+++ CS116_Labs/src/L2/Book.java
@@ -1,5 +1,14 @@
 package L2;
 
 public class Book {
+	private String title = "";
+	private String author = "";
+	private String genre = "";
+	private int isbn = 0;
+	
+	public Book() {
+		
+	}
+	
 
 }
