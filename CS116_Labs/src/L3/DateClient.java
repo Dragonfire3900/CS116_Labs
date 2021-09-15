1d860d58c91da5b61562528614350615ac33f302
tangentialcubic <tangentialcubic@gmail.com>
2021-09-15 14:03:40 -0500

    Copy paste of Date code

--- CS116_Labs/src/L3/DateClient.java
+++ CS116_Labs/src/L3/DateClient.java
@@ -1,5 +1,28 @@
 package L3;
 
-public class DateClient {
+public class DateClient
+{
+  public static void main( String [] args )
+  {
+     // add code to construct Data objects
+	 // and test and output if they are a
+	 // leap year or not
+	 
+	 // not leap year (not divisible by 4)
 
+
+	 // leap year (divisible by 4, but not by 100)
+
+
+	 // not leap year (divisible by 4 and by 100)
+
+
+	 // leap year (divisible by 4 and by 100 and by 400)
+
+
+	 // your birth year
+
+
+  }
 }
+
