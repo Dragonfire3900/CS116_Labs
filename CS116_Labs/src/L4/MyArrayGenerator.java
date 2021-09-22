5fe876fa517d554d4e82361cc07bea9751809534
August Gross <agross4@hawk.iit.edu>
2021-09-22 14:56:28 -0500

    update

--- CS116_Labs/src/L4/MyArrayGenerator.java
+++ CS116_Labs/src/L4/MyArrayGenerator.java
@@ -1,5 +1,65 @@
 package L4;
 
 public class MyArrayGenerator {
-
+	private int numberOfElements;
+	private int[] Arr;
+	
+	private int[] elem = new int[11];
+	
+	public MyArrayGenerator() {
+		numberOfElements = 0 + (int)(Math.random() * 15);
+		Arr = new int[numberOfElements];
+		for (int i = 0; i < numberOfElements; i++)
+        {
+			Arr[i] = 0 + (int)(Math.random() * 10);
+			switch(Arr[i]) {
+				case 0:
+					elem[0]++;
+					break;
+				case 1:
+					elem[1]++;
+					break;
+				case 2:
+					elem[2]++;
+					break;
+				case 3:
+					elem[3]++;
+					break;
+				case 4:
+					elem[4]++;
+					break;
+				case 5:
+					elem[5]++;
+					break;
+				case 6:
+					elem[6]++;
+					break;
+				case 7:
+					elem[7]++;
+					break;
+				case 8:
+					elem[8]++;
+					break;
+				case 9:
+					elem[9]++;
+					break;
+				case 10:
+					elem[10]++;
+					break;
+			}
+			
+        }
+	}
+	
+	public int getArrSize() {
+		return numberOfElements;
+	}
+	
+	public int[] getArr() {
+		return Arr;
+	}
+	
+	public int[] getElem() {
+		return elem;
+	}
 }
--- CS116_Labs/src/L4/WhatIsInCommon.java
+++ CS116_Labs/src/L4/WhatIsInCommon.java
@@ -1,10 +1,26 @@
 package L4;
+import java.util.Arrays;
 
 public class WhatIsInCommon {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sE = 0;
+		
+		MyArrayGenerator a = new MyArrayGenerator();
+		MyArrayGenerator b = new MyArrayGenerator();
+		int[] arrA = a.getArr();
+		int[] arrB = b.getArr();
+		
+		System.out.println("Array A: " + Arrays.toString(arrA));
+		System.out.println("Array b: " + Arrays.toString(arrB));
+		for(int i = 0; i < 11; i++) {
+			if(a.getElem()[i] > 0 && b.getElem()[i] > 0) {
+				System.out.println(i + "			" + a.getElem()[i] + "			" + b.getElem()[i]);
+				sE++;
+			}
+		}
+		System.out.println("Number of elements that are both A and B: " + sE);
+		
 	}
 
 }
