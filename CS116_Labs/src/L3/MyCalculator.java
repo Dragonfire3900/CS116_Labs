c3cdee0015d49033a36db3acb3328142cdc174f2
August Gross <agross4@hawk.iit.edu>
2021-09-21 22:12:33 -0500

    update

--- CS116_Labs/src/L3/MyCalculator.java
+++ CS116_Labs/src/L3/MyCalculator.java
@@ -4,7 +4,8 @@ public class MyCalculator {
 	private double a;
 	private double b;
 	private char opr;
-	private boolean valid = true;
+	private boolean validV = true;
+	private boolean validO = true;
 	
 	public MyCalculator(String m) {
 		String mProblem = m.replaceAll("\\s", "");
@@ -12,19 +13,21 @@ public class MyCalculator {
 			a = Character.getNumericValue(mProblem.charAt(0));
 			b = Character.getNumericValue(mProblem.charAt(2));
 		}
-		else valid = false;
+		else validV = false;
 		
 		if(mProblem.charAt(1) == '+' || mProblem.charAt(1) == '-' || mProblem.charAt(1) == '*' || mProblem.charAt(1) == '/' || mProblem.charAt(1) == '^') {
 			opr = mProblem.charAt(1);
 		}
-		else valid = false;
+		else validO = false;
 	}
 	
 	public String evaluateExpression() {
 		double r = 0;
 		
-		if (!valid) return "ERROR: Impossible to evaluate this expression.";
+		if (!validV) return "ERROR: Impossible to evaluate this expression.";
 		
+		if(!validO) return "ERROR: Unknown operator.";
+
 		switch (opr) {
 			case '+': 	r = (a + b);
 						break;
