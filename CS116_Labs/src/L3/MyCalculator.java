b6c88f55242fc87ee89c287eb3a23b5fc7efedb0
August Gross <agross4@hawk.iit.edu>
2021-09-21 21:37:49 -0500

    update

--- CS116_Labs/src/L3/MyCalculator.java
+++ CS116_Labs/src/L3/MyCalculator.java
@@ -1,5 +1,32 @@
 package L3;
 
 public class MyCalculator {
+	private double a;
+	private double b;
+	private char opr;
+	
+	public MyCalculator(String m) {
+		String mProblem = m.trim();
+		a = Character.getNumericValue(mProblem.charAt(0));
+		b = Character.getNumericValue(mProblem.charAt(2));
+		opr = mProblem.charAt(1);
+	}
+	
+	public double evaluateExpression() {
+		double r = 0;
+		switch (opr) {
+			case '+': 	r = (a + b);
+						break;
+			case '-': 	r = (a - b);
+						break; 	
+			case '*': 	r = (a * b);
+						break;  
+			case '/': 	r = (a / b);
+						break; 
+			case '^': 	r = (Math.pow(a , b));
+						break; 
+		}
+		return r;
+	}
 
 }
