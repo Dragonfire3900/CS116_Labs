bedcecec05b0d6c77f2e6c5cbffc6ba945b1ad52
August Gross <agross4@hawk.iit.edu>
2021-09-21 22:07:07 -0500

    updated

--- CS116_Labs/src/L3/MyCalculator.java
+++ CS116_Labs/src/L3/MyCalculator.java
@@ -4,16 +4,27 @@ public class MyCalculator {
 	private double a;
 	private double b;
 	private char opr;
+	private boolean valid = true;
 	
 	public MyCalculator(String m) {
-		String mProblem = m.trim();
-		a = Character.getNumericValue(mProblem.charAt(0));
-		b = Character.getNumericValue(mProblem.charAt(2));
-		opr = mProblem.charAt(1);
+		String mProblem = m.replaceAll("\\s", "");
+		if(Character.isDigit(mProblem.charAt(0)) && Character.isDigit(mProblem.charAt(2))) {
+			a = Character.getNumericValue(mProblem.charAt(0));
+			b = Character.getNumericValue(mProblem.charAt(2));
+		}
+		else valid = false;
+		
+		if(mProblem.charAt(1) == '+' || mProblem.charAt(1) == '-' || mProblem.charAt(1) == '*' || mProblem.charAt(1) == '/' || mProblem.charAt(1) == '^') {
+			opr = mProblem.charAt(1);
+		}
+		else valid = false;
 	}
 	
 	public String evaluateExpression() {
 		double r = 0;
+		
+		if (!valid) return "ERROR: Impossible to evaluate this expression.";
+		
 		switch (opr) {
 			case '+': 	r = (a + b);
 						break;
@@ -27,8 +38,8 @@ public class MyCalculator {
 						break; 
 		}
 		
-		if(r != Double.NaN) return String.valueOf(r);
-		else return "ERROR: Impossible to evaluate this expression.";
+		return String.valueOf(r);
+		
 	}
 
 }
