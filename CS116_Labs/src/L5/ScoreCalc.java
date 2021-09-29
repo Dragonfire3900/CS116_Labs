6915858adcc75f891364a9cea0164a29d56a56ef
tangentialcubic <tangentialcubic@gmail.com>
2021-09-29 14:32:57 -0500

    init on the score one

---
+++ CS116_Labs/src/L5/ScoreCalc.java
@@ -0,0 +1,16 @@
+package L5;
+
+public class ScoreCalc {
+
+	public static void main(String[] args) {
+		//Variables used for this program
+		int numScore; //The number of scores for this calculation
+		double scores[]; //The actual score values
+		double totalScore; //The total of scores jut to make sure repeated calculation doesn't occur
+		double avg = 0.0f; //The average score
+		int aboveCount = 0; //The number of scores above the average
+		
+		
+	}
+	
+}
