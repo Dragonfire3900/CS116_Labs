d2be92c9681d1cd371300bd362a252322fc97628
August Gross <agross4@hawk.iit.edu>
2021-09-29 17:28:06 -0500

    finished

--- CS116_Labs/src/L5/UnsortedData.java
+++ CS116_Labs/src/L5/UnsortedData.java
@@ -1,5 +1,28 @@
 package L5;
 
 public class UnsortedData {
-
+	public static int countOutOfPosition(double [] nL) {
+		int count = 0;
+		for (int i = 0; i < 99; i++) {
+			for (int j = i + 1; j < 100; j++) {
+				if(nL[j] < nL[i]) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+	
+	public static void bubble(double [] nL) {
+		double sw;
+		for (int i = 0; i < 99; i++) {
+			if(nL[i] > nL[i+1]) {
+				sw = nL[i];
+				nL[i] = nL[i+1];
+				nL[i+1] = sw;
+			}
+		}
+	}
+	
+	
 }
--- CS116_Labs/src/L5/UnsortedDataApp.java
+++ CS116_Labs/src/L5/UnsortedDataApp.java
@@ -1,5 +1,30 @@
 package L5;
+import java.util.*;
+import java.io.*;
 
 public class UnsortedDataApp {
-
+	public static void main(String[] args) throws FileNotFoundException {
+	    double [] fileNums = new double [100];
+		double myNum;
+		
+		Scanner askFile = new Scanner(System.in);
+	    System.out.println("What is the name of the input file?");
+	    String fileN = askFile.nextLine();
+	    askFile.close();
+	    
+	    File myFile = new File(fileN);
+	    Scanner fileScanner = new Scanner(myFile);
+	    
+	    for (int i = 0; i < 100; i++) {
+	    	myNum = fileScanner.nextDouble();
+	    	fileNums[i] = myNum;
+	    }
+	    
+	    fileScanner.close();
+	    
+	    System.out.println("Total Out of Order BEFORE Bubble = " + UnsortedData.countOutOfPosition(fileNums));
+	    UnsortedData.bubble(fileNums);
+	    System.out.println("Total Out of Order AFTER Bubble = " + UnsortedData.countOutOfPosition(fileNums));
+	    
+	  }
 }
