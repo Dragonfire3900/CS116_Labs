7d7b2ec39dda75daa9a5c75ec332d9a8517e9062
tangentialcubic <tangentialcubic@gmail.com>
2021-09-29 14:55:05 -0500

    User input for score calc

--- CS116_Labs/src/L5/ScoreCalc.java
+++ CS116_Labs/src/L5/ScoreCalc.java
@@ -1,5 +1,7 @@
 package L5;
 
+import java.util.*;
+
 public class ScoreCalc {
 
 	public static void main(String[] args) {
@@ -10,7 +12,60 @@ public class ScoreCalc {
 		double avg = 0.0f; //The average score
 		int aboveCount = 0; //The number of scores above the average
 		
+		Scanner input = new Scanner(System.in);
+		
 		
 	}
 	
+	/**
+	 * Gets a double from the user using a given scanner
+	 * 
+	 * @param input The scanner from which the double will be acquired
+	 * @param initPrompt The initial prompt for the user's input
+	 * @param repeatPrompt The prompt used when the user provides invalid input
+	 * @return The double the user gives to the program
+	 */
+	private static double getDouble(Scanner input, String initPrompt, String repeatPrompt) {
+		if (input == null) { return Double.NaN; } //User provided a null scanner
+		
+		double output;
+		
+		System.out.println(initPrompt + "\n");
+		while (true) {
+			try {
+				output = input.nextDouble();
+				return output;
+			} catch (InputMismatchException ex) {
+				System.out.println(repeatPrompt + "\n");
+			} finally {
+				input.nextLine();
+			}
+		}
+	}
+	
+	/**
+	 * Gets an int from the user using a given scanner and prompt
+	 * 
+	 * @param input The scanner from which the int will be acquired
+	 * @param initPrompt The initial prompt for the user's input
+	 * @param repeatPrompt The prompt used when the user provides invalid input
+	 * @return The int the user gives to the program
+	 */
+	private static int getInt(Scanner input, String initPrompt, String repeatPrompt) {
+		if (input == null) { return -1; } //User provided a null scanner
+		
+		int output;
+		
+		System.out.println(initPrompt + "\n");
+		while (true) {
+			try {
+				output = input.nextInt();
+				return output;
+			} catch (InputMismatchException ex) {
+				System.out.println(repeatPrompt + "\n");
+			} finally {
+				input.nextLine();
+			}
+		}
+	}
 }
