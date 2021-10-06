e353a0b3457862c0718d29c68eda3ecbdb5519a9
August Gross <agross4@hawk.iit.edu>
2021-10-06 15:05:35 -0500

    update

--- CS116_Labs/src/L6/AudioRecording.java
+++ CS116_Labs/src/L6/AudioRecording.java
@@ -1,5 +1,27 @@
 package L6;
-
+import L5.*;
+import L5.Record;
 public class AudioRecording extends Recording{
-
+	final double bitrate;
+	AudioRecording() {
+		super();
+		bitrate = 0;
+	}
+	AudioRecording(String artist, String name, int duration, double br) {
+		super(artist, name, duration);
+		if (br < 0) {
+			bitrate = 0;
+		} else {
+			bitrate = br;
+		}
+	}
+	
+	public double getBitrate() {
+		return bitrate;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s - %s [%dm%ds]", this.getArtist(), this.NAME, this.getDurationInMin(), this.getDurationInSeconds() - 60 * this.getDurationInMin());
+	}
 }
--- CS116_Labs/src/L6/VideoRecording.java
+++ CS116_Labs/src/L6/VideoRecording.java
@@ -1,5 +1,8 @@
 package L6;
-
+import L5.*;
 public class VideoRecording extends Recording{
-
+	final double framerate;
+	VideoRecording(){
+		
+	}
 }
