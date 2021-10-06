7a4aadd14ee774d8e4c131c2b873e7df134e11c5
August Gross <agross4@hawk.iit.edu>
2021-10-06 14:09:39 -0500

    vector update

--- CS116_Labs/src/L6/Vector.java
+++ CS116_Labs/src/L6/Vector.java
@@ -1,5 +1,34 @@
 package L6;
 
 public class Vector {
-
+	private double startPointX = 0;
+	private double startPointY = 0;
+	private final double endPointX;
+	private final double endPointY;
+	
+	Vector() {
+		endPointX = 0;
+		endPointY = 0;
+	}
+	Vector(double a, double b) {
+		endPointX = a;
+		endPointY = b;
+	}
+	
+	public double getStartPointX() {
+		return startPointX;
+	}
+	public double getStartPointY() {
+		return startPointY;
+	}
+	public double getEndPointX() {
+		return endPointX;
+	}
+	public double getEndPointY() {
+		return endPointY;
+	}
+	
+	public double getLength() {
+		return Math.sqrt((Math.pow(endPointX - startPointX, 2) + Math.pow(endPointY - startPointY, 2)));
+	}
 }
