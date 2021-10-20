bf638ce32255d6a2af0e46fcd45aeeb5d50aff78
August Gross <agross4@hawk.iit.edu>
2021-10-20 14:34:29 -0500

    update

--- CS116_Labs/src/L7/Sphere.java
+++ CS116_Labs/src/L7/Sphere.java
@@ -1,5 +1,22 @@
 package L7;
 
 public class Sphere extends CircleSolid {
+	
+	public Sphere() {
+		super();
+	}
+	
+	public Sphere(double r) {
+		super(r);
+	}
+	
+	public double getVolume() {
+		return (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+	}
 
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return (4.0) * Math.PI * Math.pow(radius, 2);
+	}
 }
