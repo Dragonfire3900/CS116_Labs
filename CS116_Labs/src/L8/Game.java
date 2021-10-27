71915f278df2862c07de7e9d428c02fe834910b0
August Gross <agross4@hawk.iit.edu>
2021-10-27 14:44:31 -0500

    update

--- CS116_Labs/src/L8/Game.java
+++ CS116_Labs/src/L8/Game.java
@@ -1,5 +1,30 @@
 package L8;
 
-public class Game {
+import java.util.Arrays;
+import java.util.Random;
 
+public class Game {
+	private int prizeLocation;
+	
+	Game() {
+		Random rand = new Random();
+		
+		prizeLocation = rand.nextInt((3 - 1) + 1) + 1;
+	}
+	
+	public int getPrizeLocation() {
+		return prizeLocation;
+	}
+
+	public void reset() {
+		Random rand = new Random();
+		prizeLocation = rand.nextInt((3 - 1) + 1) + 1;
+	}
+	
+	public int getOpenDoor(int p) {
+		int[] doors = {1, 2, 3};
+		Random rand = new Random();
+		doors = Arrays.stream(doors).filter(x -> x != p && x != prizeLocation).toArray();
+		return doors[rand.nextInt((doors.length - 1) + 1) + 1];
+	}
 }
