48a2c700b87858f4cf9c71e579803ca6d420f47a
tangentialcubic <tangentialcubic@gmail.com>
2021-11-03 14:45:23 -0500

    Finished the search interface

--- CS116_Labs/src/L9/SearchInterface.java
+++ CS116_Labs/src/L9/SearchInterface.java
@@ -1,5 +1,14 @@
 package L9;
 
 public interface SearchInterface {
-
+	/**
+	 * Finds the index where the target should be inserted. Assumes that the list needs to be lowest to greatest
+	 * @param data The data to search through
+	 * @param target The target to find
+	 * @param lowBound The lower bound to search in (inclusive)
+	 * @param upperBound The upper bound to search in (exclusive)
+	 * 
+	 * @return the index of where the target should be inserted
+	 */
+	public int findIndex(double[] data, double target, int lowBound, int upperBound); 
 }
