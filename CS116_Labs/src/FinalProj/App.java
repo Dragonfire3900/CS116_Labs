80013e1f2c82002af8429fd55da22d8e999884aa
tangentialcubic <tangentialcubic@gmail.com>
2021-12-07 19:59:50 -0600

    Fixed the final project duplicate

--- CS116_Labs/src/FinalProj/App.java
+++ CS116_Labs/src/FinalProj/App.java
@@ -1,5 +1,26 @@
 package FinalProj;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static UserMenu() {
+		
+	}
+	
+	public static StreamMenu() {
+		
+	}
+
+	public static String getStrInput(Scanner sc) {
+		
+	}
+
+	public static int getIntInput(Scanner sc) {
+		
+	}
 }
--- CS116_Labs/src/FinalProject/RecordingFactory.java
+++
@@ -1,7 +0,0 @@
-package FinalProject;
-
-public class RecordingFactory {
-	public static Recording Build() {
-		
-	}
-}
