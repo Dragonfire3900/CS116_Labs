b79264de8865e1ae12132e6d7875c642f9912f94
tangentialcubic <tangentialcubic@gmail.com>
2021-12-08 15:03:47 -0600

    Finished up the recording factory

--- CS116_Labs/src/FinalProj/RecordingFactory.java
+++ CS116_Labs/src/FinalProj/RecordingFactory.java
@@ -1,8 +1,29 @@
 package FinalProj;
 
+
 public class RecordingFactory {
 
-	public Recording build(String inpt) {
+	private static final int MIN_NUM_DETAILS = 4; //The minimum number of details needed to construct a record
+	protected static final String DEF_DELIM = " ";
+	
+	public static Recording build(String stringRecord, String delim) throws MalformedRecording {
+		if (stringRecord == null) { throw new NullPointerException("The string version of the record was null"); }
 		
+		String[] infoList = stringRecord.split(delim);
+		
+		if (infoList.length < RecordingFactory.MIN_NUM_DETAILS) { throw new MalformedRecording("The string recording didn't have enough info"); }
+		
+		switch(infoList.length) {
+			case 5: switch (infoList[0]) {
+						case "V": return new VideoRecording(infoList[2], infoList[1], Integer.parseInt(infoList[3]), Double.parseDouble(infoList[4]));
+						case "A": return new AudioRecording(infoList[2], infoList[1], Integer.parseInt(infoList[3]), Double.parseDouble(infoList[4]));
+						default: throw new IllegalArgumentException("Unknown string record format");
+					}
+			default: throw new MalformedRecording("Unknown string record format");
+		}
+	}
+	
+	public static Recording build(String stringRecord) {
+		return RecordingFactory.build(stringRecord, RecordingFactory.DEF_DELIM);
 	}
 }
