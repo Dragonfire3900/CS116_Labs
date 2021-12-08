9399bad9abb689ca4274b585525abf68e4f05a66
tangentialcubic <tangentialcubic@gmail.com>
2021-12-08 14:52:13 -0600

    Finished up the new recording class

--- CS116_Labs/src/FinalProj/Recording.java
+++ CS116_Labs/src/FinalProj/Recording.java
@@ -1,5 +1,74 @@
 package FinalProj;
 
 public abstract class Recording {
-
+	//Class defaults
+	protected final static String DEF_ARTIST = "UNK";
+	protected final static String DEF_NAME = "UNK";
+	protected final static int DEF_DUR = 0;
+	protected final static String DEF_DELIM = " ";
+	
+	//Attributes
+	protected String artist;
+	protected String name;
+	protected int dur; //This is in second
+	
+	public Recording() {
+		this.artist = Recording.DEF_ARTIST;
+		this.name = Recording.DEF_NAME;
+		this.dur = Recording.DEF_DUR;
+	}
+	
+	public Recording(String nArtist, String nName, int nDur) {
+		if (nArtist.isBlank()) {
+			this.artist = Recording.DEF_ARTIST;
+		} else {
+			this.artist = nArtist;
+		}
+		
+		if (nName.isBlank()) {
+			this.name = Recording.DEF_NAME;
+		} else {
+			this.name = nName;
+		}
+		
+		if (nDur <= 0) {
+			this.dur = Recording.DEF_DUR;
+		} else {
+			this.dur = nDur;
+		}
+	}
+	
+	//Accessors
+	public String getArtist() { return this.artist; }
+	
+	public String getName() { return this.name; }
+	
+	public int getDurInSeconds() { return this.dur; }
+	
+	public int getDurInMin() { return this.dur / 60; }
+	
+	//Comparators
+	public boolean equals(Recording other) {
+		return this.artist.equals(other.getArtist()) &&
+				this.name.equals(other.getName()) && 
+				this.dur == other.dur;
+	}
+	
+	public boolean equalNames(Recording other) {
+		return this.artist.equals(other.getArtist()) &&
+				this.name.equals(other.getName());
+	}
+	
+	//String manipulations
+	public void play() throws Unplayable {
+		if (this.dur == 0) {
+			throw new Unplayable(String.format("%s is unplayable", this.toString()));
+		} else {
+			System.out.println("Now playing: " + this.toString());
+		}
+	}
+	
+	public String toString() {
+		return String.format("%s - %s [%dm%ds]", this.artist, this.name, this.getDurInMin(), this.getDurInSeconds() - 60 * this.getDurInMin());
+	}
 }
